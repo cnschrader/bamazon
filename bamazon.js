e88b06818007d79a7b9e3017f937acb1540a8b28
Caitlin Schrader <caitlinschrader@caitlins-mbp.home>
2019-05-05 15:27:42 -0500

    trying to connect the database with my files

--- bamazon.js
+++ bamazon.js
@@ -1,3 +1,25 @@
 require("dotenv").config();
 
+const mysql = require("mysql");
+
+const inquirer = require("inquirer");
+
 const keys = require("./keys.js");
+
+const db = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: keys.sql.username,
+
+    password: keys.sql.password,
+    database: "bamazon_db"
+});
+
+db.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + db.threadId);
+    db.end();
+  });
+  
