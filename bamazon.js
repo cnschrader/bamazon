85b1d7e44047834a1bb8554f7fffa851a7407115
Caitlin Schrader <caitlinschrader@caitlins-mbp.home>
2019-05-06 19:19:28 -0500

    table displaying properly, inquirer prompting questions properly, proper connection to database

--- bamazon.js
+++ bamazon.js
@@ -6,6 +6,8 @@ const inquirer = require("inquirer");
 
 const keys = require("./keys.js");
 
+const { printTable } = require("console-table-printer");
+
 const db = mysql.createConnection({
     host: "localhost",
 
@@ -20,6 +22,42 @@ const db = mysql.createConnection({
 db.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + db.threadId);
-    db.end();
+    displayProduct();
+    selectProduct();
   });
+
+  function displayProduct() {
+    db.query("SELECT * FROM products", (err, res) => {
+      printTable(res);
+      
+    });
+  
+    
+  }
+
+  function selectProduct() {
+    db.query('SELECT product_name FROM products ORDER BY product_name', (err, res) => {
+      if (err) throw err;
+      const productName = res.map(row => row.product_name);
+      console.log(`Select what product you would like to purchase`);
+      inquirer.prompt([{
+        name: "product",
+        message: "Select your product",
+        type: "list",
+        choices: productName
+      },
+      {
+        name: "quantity",
+        message: "How many would you like?"
+      }
+      
+  
+      ]).then(product => {
+        console.log(product);
+        db.end();
+        
+      });
+    });
+  }
+
   
